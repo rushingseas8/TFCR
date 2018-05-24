61ddfc1e7b28d5d1af2d0a30614364b896544300
galeks2 <galeks2@illinois.edu>
2018-05-24 01:37:38 -0500

    finished basic log block without regular rotation

--- main/java/com/pffft/tfcr/blocks/BlockWood.java
+++ main/java/com/pffft/tfcr/blocks/BlockWood.java
@@ -4,11 +4,13 @@ import com.pffft.tfcr.TFCR;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
+import net.minecraft.creativetab.CreativeTabs;
 
 public class BlockWood extends Block {
 	public BlockWood(String name) {
 		super(Material.WOOD);
 		setUnlocalizedName("wood_" + name);
 		setRegistryName(TFCR.MODID, "wood_" + name);
+		setCreativeTab(CreativeTabs.BUILDING_BLOCKS);
 	}
 }
